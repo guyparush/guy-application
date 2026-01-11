e76dc7e80f1f31715aaa8ca20b2d2647fe5d1885
User <liea.david.123@gmail.com>
2026-01-11 16:49:03 +0200

    11/1/26

--- app/src/main/java/com/katza/guyapplication/DialogActivity.java
+++ app/src/main/java/com/katza/guyapplication/DialogActivity.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
 
 public class DialogActivity extends AppCompatActivity
         implements View.OnClickListener {
@@ -28,13 +28,71 @@ public class DialogActivity extends AppCompatActivity
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
 
-        sp = getSharedPreferences("details1", 0);
+        sp = getSharedPreferences("details1", MODE_PRIVATE);
+    }
+
+    public void createLoginDialog() {
+        d = new Dialog(this);
+        d.setContentView(R.layout.custom_layout);
+        d.setTitle("login");
+        d.setCancelable(true);
+
+        etUserName = d.findViewById(R.id.etUserName);
+        etPass = d.findViewById(R.id.etPassword);
+        btnCustomLogin = d.findViewById(R.id.btnDialogLogin);
+
+        btnCustomLogin.setOnClickListener(this);
+
+        // טעינת נתונים שמורים (אם קיימים)
+        String strname = sp.getString("username", null);
+        String strpass = sp.getString("pass", null);
+
+        if (strname != null && strpass != null) {
+            etUserName.setText(strname);
+            etPass.setText(strpass);
+        }
+
+        d.show();
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnLogin) {
-            // פעולה בלחיצה
+        if (v == btnLogin) {
+            createLoginDialog();
+        } else if (v == btnCustomLogin) {
+
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("username", etUserName.getText().toString());
+            editor.putString("pass", etPass.getText().toString());
+            editor.apply();
+
+            Toast.makeText(this,
+                    "username password saved",
+                    Toast.LENGTH_LONG).show();
+
+            d.dismiss();
         }
     }
 }
+
+String strname = sp.getString("username",null);
+String strpass = sp.getString("pass",null);
+if(strname!=null&&strpass!=null) {
+    etUserName.setText(strname);
+    etPass.setText(strpass);
+        }
+@Override
+public void onCliclk(View v)
+{
+    if(v==btnLogin) {
+        createLoginDialog();
+    } else if (v==btnCustomLogin) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("username",etUserName.getText().toString());
+        editor.putString("pass",etPass.getText().toStirng());
+        editor.commit();
+        Toast.makeText(this,"username password saved",toast.LENGTH_LONG).show();
+
+    }
+}
+
